00a328da8bdfe55f79dc29ef0754257178146e12
Jonathan Wakely <jwakely@redhat.com>
2014-04-29 19:41:10 +0100

    Fix English in vim_update.sh
    Fix grammatical problems in the output.

--- files/snippets/vim/vim_update.sh
+++ files/snippets/vim/vim_update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "vimrc file delivered by devassistant feature was not found and a parameter"
+    echo "vimrc file delivered by devassistant feature was not found as a parameter"
     echo "syntax is: $0 <path_to_vim_rc_dev_assistant_file>"
     exit 1
 fi
 
-echo "This procedure is used for update your current vim and improve them"
+echo "This procedure is used to update your current vim and improve them"
 PWD=`pwd`
 HOME_DIR=`echo $HOME`
 CUR_VIM="$HOME/.vimrc"
@@ -24,8 +24,8 @@ if [ -f $CUR_VIM ]; then
 fi
 sed -i '/\"BEGIN_DEVASSISTANT_1/,/\"END_DEVASSISTANT_1/d' $CUR_VIM
 echo "\"BEGIN_DEVASSISTANT_1" >> $CUR_VIM
-echo "\"Turning value devassistant to 0 you will used your already defined .vimrc file" >> $CUR_VIM
-echo "\"Turning value devassistant to 1 you will use vimrc defined by devassistant feature" >> $CUR_VIM
+echo "\"Setting value devassistant to 0 will use your existing .vimrc file" >> $CUR_VIM
+echo "\"Setting value devassistant to 1 will use the vimrc defined by the devassistant feature" >> $CUR_VIM
 echo "" >> $CUR_VIM
 echo "let devassistant=0" >> $CUR_VIM
 echo "if devassistant==1" >> $CUR_VIM
@@ -34,9 +34,9 @@ echo "endif" >> $CUR_VIM
 echo "\"END_DEVASSISTANT_1" >> $CUR_VIM
 echo ""
 echo ""
-echo "Updating of ~/.vimrc file was successfull"
+echo "Updating of ~/.vimrc file was successful"
 echo "************************************"
-echo "For turning on devassistant vimrc file"
+echo "To turn on the devassistant vimrc file"
 echo "open ~/.vimrc file and change value \"let devassistant=0\" to \"let devassistant=1\""
 echo "************************************"
-echo "After pressing F1 you see help about commands etc."
+echo "After pressing F1 you will see help about commands etc."
