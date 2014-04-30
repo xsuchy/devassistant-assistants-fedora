34023d6bac9a6cd60bab72c916213d96ce28dcfa
Jonathan Wakely <jwakely@redhat.com>
2014-04-30 13:10:39 +0100

    Don't use bzero and bcopy.
    These functions are deprecated and no longer part of POSIX.
    Use memset and memcpy instead.

--- files/crt/c/client.c
+++ files/crt/c/client.c
@@ -33,21 +33,19 @@ int main(int argc, char *argv[])
         fprintf(stderr,"ERROR, no such host\n");
         exit(1);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
     printf("Please enter the message: ");
-    bzero(buffer,256);
+    memset(buffer, 0, 256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
          error("ERROR writing to socket");
-    bzero(buffer,256);
+    memset(buffer, 0, 256);
     n = read(sockfd,buffer,255);
     if (n < 0) 
          error("ERROR reading from socket");
