3df3bb11217ca2430f06b0b0eac2ddd7b5e1a37a
Jonathan Wakely <jwakely@redhat.com>
2014-04-30 13:12:00 +0100

    Fix inconsistent indentation.

--- files/crt/c/client.c
+++ files/crt/c/client.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 
 void error(const char *msg)
 {
@@ -21,12 +21,12 @@ int main(int argc, char *argv[])
 
     char buffer[256];
     if (argc < 3) {
-       fprintf(stderr,"Usage: %s hostname port\n", argv[0]);
-       exit(1);
+        fprintf(stderr,"Usage: %s hostname port\n", argv[0]);
+        exit(1);
     }
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         error("ERROR opening socket");
     server = gethostbyname(argv[1]);
     if (server == NULL) {
@@ -37,18 +37,18 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;
     memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
     printf("Please enter the message: ");
     memset(buffer, 0, 256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
+    if (n < 0)
+        error("ERROR writing to socket");
     memset(buffer, 0, 256);
     n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
+    if (n < 0)
+        error("ERROR reading from socket");
     printf("%s\n",buffer);
     close(sockfd);
     return 0;
