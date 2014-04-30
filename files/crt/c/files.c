18198c34570c44bc9170787391010090f9f60986
Jonathan Wakely <jwakely@redhat.com>
2014-04-30 14:06:51 +0100

    Use whitespace consistently.

--- files/crt/c/files.c
+++ files/crt/c/files.c
@@ -8,9 +8,9 @@ int writeFile()
 	FILE * f;
 	const char * str = "This is a simple example showing how to write a string to a file.";
 	f = fopen("/tmp/fileTest","wt");
-	if ( f == NULL)
+	if (f == NULL)
 		return 1;
-	fprintf(f,"%s\n", str);
+	fprintf(f, "%s\n", str);
 	fflush(f);
 	fclose(f);
 	return 0;
@@ -23,21 +23,21 @@ int readFile()
 	char * tmpBuf;
 	long lSize;
 	f = fopen("/tmp/fileTest","rt");
-	if ( f == NULL)
+	if (f == NULL)
 		return 1;
 	fseek (f, 0, SEEK_END);
 	lSize = ftell(f);
-	printf("Length of file is: %ld\n",lSize);
+	printf("Length of file is: %ld\n", lSize);
 	fseek (f, 0, SEEK_SET);
 	tmpBuf = (char *)malloc(sizeof(char)*lSize);
-	size_t n = fread(tmpBuf,1,lSize,f);
+	size_t n = fread(tmpBuf, 1, lSize, f);
 	if (n == lSize)
 	{
-		printf("Ouput from file is: %s\n",tmpBuf);
+		printf("Ouput from file is: %s\n", tmpBuf);
 	}
 	else
 	{
-		printf("Reading failed %zd\n",n);
+		printf("Reading failed %zd\n", n);
 		return 1;
 	}
 	free(tmpBuf);
@@ -49,14 +49,14 @@ int main()
 {
 	printf("Hello World\n");
 
-	if(writeFile()==0)
+	if (writeFile( )== 0)
 		printf("Writing to file was successfull\n");
 	else
 	{
 		printf("Writing to file failed\n");
 		exit(1);
 	}
-	if(readFile()==0)
+	if (readFile() == 0)
 		printf("Reading from file was successfull\n");
 	else
 	{
